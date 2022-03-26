f4a5dc62c8d90b94f263844e6d956232b110444e
underpig <chasepgraham@icloud.com>
2022-03-25 14:14:18 -1000

    update installation process

--- src/install.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-mkdir "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\widgets"
-xcopy widgets.bat "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\widgets"
-xcopy widgets.vbs "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
