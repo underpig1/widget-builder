4c5ac6df2a046464e077ff812a5c6f5fa4b710c2
underpig <chasepgraham@icloud.com>
2022-03-25 16:58:15 -1000

    update install process

--- install.bat
+++ install.bat
@@ -1,5 +1,6 @@
 @echo off
 mkdir "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\widgets"
+attrib +h "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\widgets" /s /d
 xcopy src\widgets.bat "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\widgets"
 xcopy src\widgets.vbs "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\"
 npm install -g
