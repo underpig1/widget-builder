a2f51d370f9c06280ffc659b129fd2a595627141
underpig <chasepgraham@icloud.com>
2022-04-06 21:34:34 -1000

    update install process for modules

--- src/install.js
+++ src/install.js
@@ -3,15 +3,19 @@ const fs = require("fs-extra")
 const pkg = require(path.join(__dirname, "config.json"))
 const exec = require("child_process").exec
 
-if (pkg.hasOwnProperty("requirements")) {
-  for (var req of pkg.requirements) {
-    exec.exec("npm install -g " + req, (err, stdout, stderr) => {
-      if (err) {
-        console.error(err)
-        return
-      }
-    })
-  }
-}
+var target_dir = process.env.APPDATA + "\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\widgets\\" + pkg.name
 
-fs.copy(__dirname,  process.env.APPDATA + "\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\widgets\\" + pkg.name).then(() => { console.log("Successfully installed " + pkg.name + "! Use 'widgets list' to see all enabled widgets") }).catch(err => console.error(err))
+fs.copy(__dirname,  target_dir).then(() => { console.log("Successfully installed " + pkg.name + "! Use 'widgets list' to see all enabled widgets") }).catch(err => console.error(err))
+
+setTimeout(() => {
+  if (pkg.hasOwnProperty("requirements")) {
+    for (var req of pkg.requirements) {
+      exec.exec("cd " + target_dir + " & npm install " + req, (err, stdout, stderr) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+      })
+    }
+  }
+}, 1000)
--- widgets/spotify-listener/renderer.js
+++ widgets/spotify-listener/renderer.js
@@ -1,34 +1,10 @@
-var SpotifyWebApi = require("spotify-web-api-node")
+var playback = require("windows.media.playback")
 
-var spotifyApi = new SpotifyWebApi({
-  clientID: "34e459a11ee64d4bb64624975e9f0cdf",
-  redirectUri: "http://localhost:3000/spotify/callback",
-  clientSecret: "900d19e498dd4535bd884c119b4794cf"
-})
-
-const spotifyLogin = (req, res) => {
-  const scopes = ['user-read-private', 'user-read-email', 'user-read-currently-playing', 'user-read-playback-state', 'user-modify-playback-state']
-  const authorizeURL = spotifyAuth.createAuthorizeURL(scopes)
-  res.redirect(authorizeURL)
-}
-
-const spotifyCallback = async (req, res) => {
-  try {
-    const code = req.query.code
-    const auth = await spotifyApi.authorizationCodeGrant(code)
-    const spotifyApi = new SpotifyWebApi({ accessToken: auth.body.access_token })
-    spotifyApi.setAccessToken(data.body["access_token"])
-    res.redirect(`${process.env.LOCAL_URI}home`)
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-// progress_time = document.getElementById("progress-time")
-// progress_indicator = document.getElementById("progress-indicator")
-// song_title = document.getElementById("song-title")
-// song_artist = document.getElementById("song-artist")
-// album_cover = ".\\media\\album-cover.jpg"
+progress_time = document.getElementById("progress-time")
+progress_indicator = document.getElementById("progress-indicator")
+song_title = document.getElementById("song-title")
+song_artist = document.getElementById("song-artist")
+album_cover = ".\\media\\album-cover.jpg"
 
 spotifyApi.getMyCurrentPlaybackState()
   .then(function(data) {
